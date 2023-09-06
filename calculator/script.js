0a3f17ed586f4b26e3c91efc38751afda6a19974
ayxan-mirzeyev-7674 <64847501+ayxan-mirzeyev-7674@users.noreply.github.com>
2023-09-06 18:22:50 +0400

    Calculator app

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,15 @@
+
+result = document.getElementById("result");
+
 function btnClicked(func){
-    console.log(func);
+    if (func != '=' && func != 'C'){
+        result.innerHTML += func;
+    }  
+    if (func == "="){
+        result.innerHTML = eval(result.innerHTML);
+    }
+    if (func == "C"){
+        result.innerHTML = ""
+    }
+
 }
